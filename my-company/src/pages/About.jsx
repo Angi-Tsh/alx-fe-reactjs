5d95e4a480a2bca161214ad910bea6fbae8d3788
Angello <kktshoga@gmail.com>
2025-07-25 11:30:46 +0900

    Remove .git

---
+++ my-company/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>What we do, why we do it and our promise to you.</p>
+    </div>
+  );
+};
+
+export default About;
