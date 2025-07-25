f5c9ec90371e49eefcb96023705947ae983388af
Angello <kktshoga@gmail.com>
2025-07-25 11:30:46 +0900

    Remove .git

---
+++ my-company/src/pages/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <p>What we can offer you.</p>
+    </div>
+  );
+};
+
+export default Services;
