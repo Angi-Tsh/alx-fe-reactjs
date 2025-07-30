ff6454bf93970ec31a33d5a3609c458f8d232181
Angello <kktshoga@gmail.com>
2025-07-30 16:02:55 +0900

    Add code to DeleteRecipe component

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { useRecipeStore } from './recipeStore'; 
+import { useRecipeStore } from './useRecipeStore'; 
 import { Recipe } from '../types'; 
 
 const EditRecipeForm = () => {
