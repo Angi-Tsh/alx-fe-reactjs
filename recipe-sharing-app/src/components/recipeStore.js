b1082b8f051fb1a0f5026fb49dd7350250d442fd
Angello <kktshoga@gmail.com>
2025-07-30 15:40:32 +0900

    Enhancing the Recipe Sharing Application with Detailed Recipe Management

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,3 +1,4 @@
+import { use } from 'react';
 import create from 'zustand'
 
 const useRecipeStore = create(set => ({
@@ -6,4 +7,20 @@ const useRecipeStore = create(set => ({
   setRecipes: (recipes) => set({ recipes })
 }));
 
-export default useRecipeStore;
+const addRecipe = (addRecipeecipe) => {
+  useRecipeStore.getState().addRecipe(recipe); }
+
+const deleteRecipe = (deleteRecipe) => {
+    useRecipeStore.setState(state => ({
+        recipes: state.recipes.filter(recipe => recipe.id !== deleteRecipe.id)
+    }));
+    }
+const updateRecipe = (updatedRecipe) => {
+    useRecipeStore.setState(state => ({
+        recipes: state.recipes.map(recipe =>
+            recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+        )
+    }));
+}
+
+export { useRecipeStore, addRecipe, deleteRecipe, updateRecipe };
