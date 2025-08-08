351b3a81706d957c5a66c582fe1c12f0bf306a31
Angello <kktshoga@gmail.com>
2025-08-08 15:50:15 +0200

    Resolved importing variable to App.jsx file

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import VITE_APP_GITHUB_API_KEY from './env'
+import React from 'react';
 
 function App() {
   const [count, setCount] = useState(0)
