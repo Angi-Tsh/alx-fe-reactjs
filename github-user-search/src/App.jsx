708d073d10674c520196ece76bd05f9c209d888c
Angello <kktshoga@gmail.com>
2025-08-10 10:11:16 +0200

    Capitalise Search.jsx file name

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react';
-import Search from './components/Search'; 
+import Search from './components/search'; 
 import { fetchUserData } from './services/githubService';
 import { data } from 'react-router-dom';
 
