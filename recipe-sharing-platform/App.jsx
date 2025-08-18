a9e6ec91f5fe8ea679bc6cd14fc3c141a7b0d7d1
Angello <kktshoga@gmail.com>
2025-08-18 12:53:40 +0200

    Syntax correction

--- recipe-sharing-platform/App.jsx
+++ recipe-sharing-platform/App.jsx
@@ -1,9 +1,10 @@
-import react from "react"
+import react from "react";
+import HomePage from "./src/components/HomePage";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <HomePage />
+    </>
   )
 }
